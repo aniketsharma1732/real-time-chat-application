135f7ee2c5afffd8a537c460e7173f584466de21
aniketsharma1732 <156927584+aniketsharma1732@users.noreply.github.com>
2025-07-04 18:21:17 +0530

    final commit 4

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Use GitHub-specific base only when building for production and deploying to GitHub Pages
-const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+
 
 export default defineConfig({
   plugins: [react()],
-  base: isGitHubPages ? '/real-time-chat-application/' : '/',
+  
 });
